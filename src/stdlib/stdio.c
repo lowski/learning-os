133c57fe1b6ff31db4fd325dd139a42600f184b2
Leonard von Lojewski <dev@vonlojewski.me>
2022-12-04 20:09:12 +0100

    refactor: extract and rename dbgu memory map

--- src/stdlib/stdio.c
+++ src/stdlib/stdio.c
@@ -2,83 +2,23 @@
 #include "str.h"
 #include "stdarg.h"
 
-#define DBGU 0xFFFFF200 // Figure 8-1
-
-struct dbgu_mm_cr {
-    //     unsigned int cr; // control; WO
-    // bitfield from 26.5.1
-    unsigned int cr_unused0: 2;
-    unsigned int cr_rstrx: 1;
-    unsigned int cr_rsttx: 1;
-    unsigned int cr_rxen: 1;
-    unsigned int cr_rxdis: 1;
-    unsigned int cr_txen: 1;
-    unsigned int cr_txdis: 1;
-
-    unsigned int cr_rststa: 1;
-    unsigned int cr_unused1: 7;
-    unsigned short cr_unused2;
-};
-
-// DBGU memory map -> Table 26-2
-struct dbgu_mm {
-    unsigned int cr; // mode; WO
-    unsigned int mr; // mode; RW
-    unsigned int ier; // interrupt enable; WO
-    unsigned int idr; // interrupt disable; WO
-    unsigned int imr; // interrupt mask; RO
-
-//    unsigned int sr; // status register; RO
-//  bitfield from 26.5.6
-    unsigned int sr_rxrdy: 1; // rx ready
-    unsigned int sr_txrdy: 1; // tx ready
-    unsigned int sr_unused0: 1;
-    unsigned int sr_endrx: 1; // end of rx transfer
-    unsigned int sr_endtx: 1; // end of tx transfer
-    unsigned int sr_ovre: 1; // overrun error
-    unsigned int sr_frame: 1; // frame error
-    unsigned int sr_pare: 1; // parity error
-
-    unsigned int sr_unused1: 1;
-    unsigned int sr_txempty: 1; // transmitter empty
-    unsigned int sr_unused2: 1;
-    unsigned int sr_txbufe: 1; // tx buffer empty
-    unsigned int sr_rxbuff: 1; // rx buffer full
-    unsigned int sr_unused3: 3;
-
-    unsigned char sr_unused4;
-    unsigned int sr_unused5: 6;
-    unsigned int sr_commtx: 1; // debug communication channel write status
-    unsigned int sr_commrx: 1; // debug communication channel read status
-
-    unsigned int rhr; // receive holding; RO
-    unsigned int thr; // transmit holding; WO
-    unsigned int brgr; // baud rate generator; RW
-    unsigned int unused0[6];
-    unsigned int cidr; // chip id; RO
-    unsigned int exid; // extension; RO
-
-    // some reserved space is left out
-};
-
-static volatile struct dbgu_mm * const dbgu = (struct dbgu_mm *)DBGU;
-
 // enable DBGU transmitter (26.4.3)
 void enable_dbgu(void) {
-    dbgu->cr = (1 << 6) | (1 << 4); // for offset see dbgu_mm_cr
+    dbgu->control = (1 << 6) | (1 << 4); // for offset see dbgu_mm_cr
+    dbgu->interrupt_enable = dbgu->interrupt_mask | MASK_DBGU_IRQ_RXRDY;
 }
 
 void transmit_byte(unsigned char byte) {
-    while (dbgu->sr_txrdy != 1);
+    while (dbgu->status.txrdy != 1);
 
-    dbgu->thr = byte;
+    dbgu->transmit_holding = byte;
 }
 
 unsigned int receive_byte() {
     // receiver overrun is being ignored.
-    while (dbgu->sr_rxrdy != 1);
+    while (dbgu->status.rxrdy != 1);
 
-    return dbgu->rhr;
+    return dbgu->receive_holding;
 }
 
 void transmit_string(const char* str) {
@@ -86,7 +26,7 @@ void transmit_string(const char* str) {
     for (int i = 0; i < len; ++i) {
         transmit_byte(str[i]);
     }
-    while (dbgu->sr_txempty != 1);
+    while (dbgu->status.txempty != 1);
 }
 
 const char alphabet[62] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -174,5 +114,5 @@ void printf(const char* fmt, ...) {
 
     va_end(args);
 
-    while (dbgu->sr_txempty != 1);
+    while (dbgu->status.txempty != 1);
 }
--- src/stdlib/stdio.h
+++ src/stdlib/stdio.h
@@ -1,6 +1,8 @@
 #ifndef LEARNING_OS_SERIAL_H
 #define LEARNING_OS_SERIAL_H
 
+#include "../memory/memory_map.h"
+
 void enable_dbgu(void);
 void printf(const char*, ...);
 
