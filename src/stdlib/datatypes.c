c3e5eda4a8d5f63c8e401f9798a7e5b68dd52367
Leonard von Lojewski <dev@vonlojewski.me>
2022-12-04 23:19:46 +0100

    feat: add ringbuffer implementation for later use

---
+++ src/stdlib/datatypes.c
@@ -0,0 +1,41 @@
+#include "datatypes.h"
+
+struct RingBuffer RingBuffer_create(unsigned int size) {
+    unsigned int buffer[size];
+    struct RingBuffer rb;
+    rb.buffer = buffer;
+    rb.size = size;
+    rb.head = 0;
+    rb.length = 0;
+    return rb;
+}
+
+void RingBuffer_push(struct RingBuffer *rb, unsigned int item) {
+    if (rb->length == rb->size) {
+        rb->head++;
+        rb->length--;
+    }
+    unsigned int i = rb->head + rb->length;
+    if (i > rb->size) {
+        i -= rb->size; // no modulo to circumvent compiler weirdness
+    }
+    rb->buffer[i] = item;
+    rb->length++;
+}
+
+unsigned int RingBuffer_pop(struct RingBuffer *rb) {
+    if (rb->length == 0) {
+        return 0;
+    }
+    unsigned int res = rb->buffer[rb->head];
+    rb->length--;
+    rb->head++;
+    if (rb->head >= rb->size) {
+        rb->head -= rb->size;
+    }
+    return res;
+}
+
+unsigned int RingBuffer_empty(struct RingBuffer *rb) {
+    return rb->length == 0;
+}
---
+++ src/stdlib/datatypes.h
@@ -0,0 +1,16 @@
+#ifndef LEARNING_OS_DATATYPES_H
+#define LEARNING_OS_DATATYPES_H
+
+struct RingBuffer {
+    unsigned int *buffer;
+    unsigned int size;
+    unsigned int head;
+    unsigned int length;
+};
+
+struct RingBuffer RingBuffer_create(unsigned int size);
+void RingBuffer_push(struct RingBuffer* rb, unsigned int item);
+unsigned int RingBuffer_pop(struct RingBuffer *rb);
+unsigned int RingBuffer_empty(struct RingBuffer *rb);
+
+#endif //LEARNING_OS_DATATYPES_H
