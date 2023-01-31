a5beae04ed1e20725f6b0a6fcc2717cefbfd3e25
Leonard von Lojewski <dev@vonlojewski.me>
2023-01-31 10:34:44 +0100

    chore: make run.sh multi-platform

--- run.sh
+++ run.sh
@@ -3,22 +3,69 @@
 PROJECT_PATH="$(cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P)/"
 cd $PROJECT_PATH
 
+DOCKER_QEMU_COMMAND=(docker run -it -v "$(pwd)/kernel.elf:/kernel" --name qemu --rm ghcr.io/lowski/qemu-portux-920t:latest)
+DOCKER_ARM_TOOLCHAIN_PREFIX=(docker run --user "$(id -u)":"$(id -g)" -v "$(pwd)":"$(pwd)" --rm --name make -w "$(pwd)" ghcr.io/lowski/gcc-arm-none-eabi arm-none-eabi)
+DOCKER_MAKE_COMMAND=(docker run --user "$(id -u)":"$(id -g)" -v "$(pwd)":"$(pwd)" --rm --name make -w "$(pwd)" ghcr.io/lowski/gcc-arm-none-eabi make)
+
+MAKE=make
+ARM_TOOLCHAIN_PREFIX=arm-none-eabi
+QEMU=qemu-system-arm
+
+if [[ $(uname -s) == 'Darwin' ]]; then
+  ARM_TOOLCHAIN_PREFIX=arm-unknown-linux-gnueabi
+fi
+
+USE_NATIVE_MAKE="$(command -v make)"
+USE_NATIVE_ARM_TOOLCHAIN="$(command -v $ARM_TOOLCHAIN_PREFIX-gcc)"
+USE_NATIVE_QEMU="$(command -v $QEMU)"
+
+if [[ -n $USE_NATIVE_QEMU ]]; then
+  USE_NATIVE_QEMU=$($QEMU -machine help | grep "portux920t")
+fi
+
+FORCE_DOCKER=false
+if [[ $1 == '--docker' || $1 == '-d' ]]; then
+  shift 1
+  FORCE_DOCKER=true
+fi
+
+#echo $USE_NATIVE_MAKE
+#echo $USE_NATIVE_QEMU
+#echo $USE_NATIVE_ARM_TOOLCHAIN
+#exit
+
+if [[ -z "$USE_NATIVE_MAKE" ]] || [[ $FORCE_DOCKER == true ]]; then
+  MAKE=(${DOCKER_MAKE_COMMAND[@]})
+fi
+if [[ -z "$USE_NATIVE_QEMU" ]] || [[ $FORCE_DOCKER == true ]]; then
+  QEMU=(${DOCKER_QEMU_COMMAND[@]})
+fi
+if [[ -z "$USE_NATIVE_ARM_TOOLCHAIN" ]] || [[ $FORCE_DOCKER == true ]]; then
+  ARM_TOOLCHAIN_PREFIX=(${DOCKER_ARM_TOOLCHAIN_PREFIX[@]})
+fi
+
 if [[ $@ == *' --clean' || $@ == *' -c' || $@ == *' -cm' ]]; then
-  make clean || exit
+  $MAKE clean || exit
 fi
 if [[ $@ == *' --make' || $@ == *' -m' || $@ == *' -cm' ]]; then
-  make kernel.elf || exit
+  $MAKE kernel.elf || exit
+fi
+
+if [[ ! -f "kernel.elf" ]]; then
+  echo ""
+  echo "No kernel.elf found. Exiting..."
+  exit 1
 fi
 
 if [[ $1 == 'objdump' ]]; then
   shift 1
-  arm-unknown-linux-gnueabi-objdump -fhd kernel.elf $@
-elif [[ $1 == 'docker' ]]; then
-  docker run -it -v "$(pwd)/kernel.elf":/kernel --name qemu --rm ghcr.io/lowski/qemu-portux-920t:latest
-elif [[ $1 == 'docker-gdb' ]]; then
-  docker run -it -p 4444:4444 -v "$(pwd)/kernel.elf":/kernel --name qemu --rm ghcr.io/lowski/qemu-portux-920t:latest -nographic -M portux920t -m 64M -kernel /kernel -S -gdb tcp::4444
+  $ARM_TOOLCHAIN_PREFIX-objdump -fhd kernel.elf $@
+elif [[ $1 == 'qemu' ]]; then
+  $QEMU
+elif [[ $1 == 'qemu-gdb' ]]; then
+  $QEMU -nographic -M portux920t -m 64M -kernel /kernel -S -gdb tcp::4444
 elif [[ $1 == 'gdb' ]]; then
-  arm-unknown-linux-gnueabi-gdb -ex "target remote :4444" kernel.elf
+  $ARM_TOOLCHAIN_PREFIX-gdb -ex "target remote :4444" kernel.elf
 else
-  echo "Command \"$1\" not recognized. Supported: docker, docker-gdb, gdb."
+  echo "Command \"$1\" not recognized. Supported: qemu, qemu-gdb, objdump, gdb."
 fi
